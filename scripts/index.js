987bc91233221d0ed34b8bb93a854cc60f3a6b88
jacob-bunker <jakebnkr@gmail.com>
2022-09-15 19:09:23 -0700

    website

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // Select the HTML element to manipulate
 const date1 = document.querySelector("#date1");
 // Try to complete the method with options
@@ -11,4 +12,19 @@ try {
 	date1.innerHTML = `<span class="highlight">${new Date().toLocaleDateString("en-UK", options)}</span>!`;
 } catch (e) {
 	alert("Error with code or your browser does not support Locale");
+=======
+// Select the HTML element to manipulate
+const date1 = document.querySelector("#date1");
+// Try to complete the method with options
+try {
+	const options = {
+		weekday: "long",
+		day: "numeric",
+		month: "long",
+		year: "numeric"
+	};
+	date1.innerHTML = `<span class="highlight">${new Date().toLocaleDateString("en-UK", options)}</span>!`;
+} catch (e) {
+	alert("Error with code or your browser does not support Locale");
+>>>>>>> 1176bd6 (website)
 }

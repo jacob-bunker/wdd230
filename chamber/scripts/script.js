23cb06436779ae78167dff03acd7c1f6e4cf2fdd
jacob-bunker <jakebnkr@gmail.com>
2022-10-08 21:25:30 -0700

    page 1

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -3,3 +3,8 @@ const currentDateField = document.querySelector("#todaysdate");
 const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
 currentDateField.innerHTML = `<em>${fulldate}</em>`;
+
+
+function toggleMenu(){
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
